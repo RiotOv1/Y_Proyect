17b329d180a2edd8185da3acea19c948381fcd8d
gisel <gisel@Abril>
2025-04-10 16:58:26 -0600

    Validaciones al momento de crear cuenta, ya se guarda la foto de perfil, y tanto la foto como el usuario estan en todas las ventanas

--- src/main/java/com/mycompany/proyect_y/Inicio.java
+++ src/main/java/com/mycompany/proyect_y/Inicio.java
@@ -355,10 +355,7 @@ ps.setBinaryStream(10, fis, (int) archivoImagenSeleccionado.length());
         return true;
         
    }
-    
- 
-
-         
+   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
